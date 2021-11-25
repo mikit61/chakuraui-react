b1b7ed65e16d856568f81f7b3d3d4b0e156b3eb3
mikit61 <94412432+mikit61@users.noreply.github.com>
2021-11-25 09:23:21 +0900

    セクション12: 実践アプリ作成(Chakra UI)

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,30 @@
-import { memo, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
+import { UserCard } from "../organism/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理画面ページです</p>;
+  const { getUsers, users, loading } = useAllUsers();
+  useEffect(() => getUsers(), [getUsers]);
+  return (
+    <>
+      {loading ? (
+        <Center height="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap spacing="30px" p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
